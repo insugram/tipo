3da7f2eb40b88e800e013377deab9c5d314164f4
infogram91 <146999350+infogram91@users.noreply.github.com>
2023-11-09 00:14:12 -0300

    colina.php

--- colina.php
+++ colina.php
@@ -1,7 +1,7 @@
 <?php
 // URL de destino real
-$urlDestino = 'https://infogram91.github.io/tipo/';
+$urlDestino = 'https://infogram91.github.io/colina.php';
 
 // Encabezados de redirección para enmascarar la URL
-header("Location: https://infogram91.github.io/colina.php");
+header("Location: https://infogram91.github.io/tipo");
 ?>
