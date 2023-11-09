d1b94d67db06bce822c8e623bbe116cd47d8150b
infogram91 <146999350+infogram91@users.noreply.github.com>
2023-11-09 00:16:41 -0300

    Delete colina.php

--- colina.php
+++
@@ -1,7 +0,0 @@
-<?php
-// URL de destino real
-$urlDestino = 'https://infogram91.github.io/colina.php';
-
-// Encabezados de redirección para enmascarar la URL
-header("Location: https://infogram91.github.io/tipo");
-?>
